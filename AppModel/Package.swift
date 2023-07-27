a8b27c589293c3920f8a14de4dd51d3137aa9f4e
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-07-27 22:58:36 +0600

    rename model to app model

--- AppModel/Package.swift
+++ AppModel/Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Model",
+    name: "AppModel",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Model",
-            targets: ["Model"]),
+            name: "AppModel",
+            targets: ["AppModel"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Model",
+            name: "AppModel",
             dependencies: []),
         .testTarget(
-            name: "ModelTests",
-            dependencies: ["Model"]),
+            name: "AppModelTests",
+            dependencies: ["AppModel"]),
     ]
 )
