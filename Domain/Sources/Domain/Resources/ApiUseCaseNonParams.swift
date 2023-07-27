794a8058e7fae56e7706d668652ae39d6ebc1e80
Kamrul Hasan <kamrulhasan3288@gmail.com>
2023-07-27 22:40:35 +0600

    Add Resources in Model

---
+++ Domain/Sources/Domain/Resources/ApiUseCaseNonParams.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kamrul Hasan on 27/7/23.
+//
+
+import Foundation
+protocol ApiUseCaseNonParams{
+    associatedtype T
+    //func execute()async -> Result<T,ApiClientError>
+}
